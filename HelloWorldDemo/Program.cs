f5f886298b1090121b36979b998a3586fcd44052
MOHD ABDUL <RAHEEM.MOHDABDUL@thomsonreuters.com>
2018-10-04 20:53:22 +0530

    Add project files.

---
+++ HelloWorldDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorldDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("What is your name ?");
+            string name=Console.ReadLine();
+            Console.WriteLine("Hello, {0} ",name);
+            Console.ReadKey();
+        }
+    }
+}
